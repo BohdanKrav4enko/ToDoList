b81f9f8cd66093059c636fcd78fc80e8d8adcc0d
BohdanKrav4enko <bohdan.krav4enko@gmail.com>
2025-04-03 23:11:39 +0300

    перевел на Redux

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,14 @@ import {createRoot} from 'react-dom/client'
 import {App} from "./App.tsx";
 import './styled.css';
 import {GlobalStyle} from "./style/GlobalStyle.tsx";
+import {Provider} from "react-redux";
+import {store} from "./components/tasks/model/store.ts";
 
 createRoot(document.getElementById('root')!).render(
     <>
         <GlobalStyle/>
+        <Provider store={store}>
         <App/>
+        </Provider>
     </>
 )
